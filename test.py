2464ecbc80f43ef65b5baa0a88d311cba7188411
Marijn Sluijs <marijnsluijs2001@gmail.com>
2021-12-01 17:31:49 +0100

    test

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
+# Import libraries
 from PIL import Image
 from numpy import asarray
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
 
-# Show image
-im1 = Image.open('Tubulins_II/01796.tif')
-im1.show()
+# Create grayscale matrix
+img_matrix = cv2.imread('00001.tiff', cv2.IMREAD_UNCHANGED)
 
-# Output image as matrix
-matrix1 = asarray(im1)
-mat1 = np.matrix(matrix1, dtype=np.int64)
+#### Show coordinates of high intensity spots
+# top-left corner is (0,0)
+# j = x, i = y
+min_intensity = 900
+k=0
+for i in range(0,255):
+        for j in range(0,255):
+            if img_matrix[i][j] > min_intensity:
+                k = k+1
+                print("(%s, %s)" % (j, i))
 
-with open('outfile.txt','wb') as f:
-    for line in mat1:
-        np.savetxt(f, line, fmt='%.0f')
+# Print amount of high intensity spots
+print(k)
 
-
-from skimage import io
-im = io.imread('Tubulins_I/00500.tif')
-print(im)
+# Create and show image from matrix
+image = cv2.convertScaleAbs(img_matrix, alpha=0.22)
+cv2.imshow("image", image)
+cv2.waitKey()
